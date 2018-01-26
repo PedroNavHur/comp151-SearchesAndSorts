230b9c5fb1a2444ca58333ee681ef5fe19ccfbff
Pedro Navarrete <pnavarrete@harding.edu>
2018-01-26 10:50:26 -0600

    Driver

--- SearchesAndSorts/BinarySearch.cpp
+++ SearchesAndSorts/BinarySearch.cpp
@@ -6,27 +6,48 @@ using namespace std;
 int BinarySearch(int[], int, int);
 
 int main() {
+
+	//int driver[50];
+	//int num;
+	//cout << "Input your number of elements . [1-50]";
+	//cin >> num;
+
+	//cout << "Input your numbers, separate them by space";
+	//for (size_t i = 0; i < num; i++){
+	//	cin >> driver[i];
+	//}
+
 	int test[] = {
-		1,
-		3,
-		6,
-		8,
+		10,
 		15,
-		46,
-		57,
-		63,
-		72,
-		87,
-		91};
+		25,
+		35,
+		40,
+		45,
+		50,
+		60,
+		70,
+		75,
+		85,
+		90,
+		95,
+		100 };
 
-	int size = 11;
-	int target = 72;
+	int size = 14;
+	int target;
 
-	int index = BinarySearch(test, size, target);
-	cout << index;
+	while (target != -1) {
+		cout << "What's your target?";
+		cin >> target;
 
+		int index = BinarySearch(test, size, target);
+		if (index == -1) {
+			"Target Not Found";
+		}
 
+		cout << "Target found at Index: " << index << endl;
 
+	}
 }
 
 int BinarySearch(int array[], int size, int target) {
